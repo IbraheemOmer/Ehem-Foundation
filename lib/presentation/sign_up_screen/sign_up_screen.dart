1130d1d0a3b25cd1be1359f904d5c65d837701b1
Ibraheem Omer <ibraheemomer.01@gmail.com>
2024-01-16 18:48:20 +0500

    Added scroll setting in sign_up_screen.dart

--- lib/presentation/sign_up_screen/sign_up_screen.dart
+++ lib/presentation/sign_up_screen/sign_up_screen.dart
@@ -83,7 +83,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            resizeToAvoidBottomInset: false,
+
             body: Form(
                 key: _formKey,
                 child: SizedBox(
